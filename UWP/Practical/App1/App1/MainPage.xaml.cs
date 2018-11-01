031938b74ef92e1e816a4d69d64c51fb649c8f5b
longstl <jonnykhanh1102@gmail.com>
2018-11-01 10:07:28 +0700

    Bai Thi

---
+++ UWP/Practical/App1/App1/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            collection = new ObservableCollection<RootObject>();
+            this.DataContext = this;
+        }
+
+        public ObservableCollection<RootObject> collection { get; set; }
+
+        private async void MainPage_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (NetworkInterface.GetIsNetworkAvailable())
+                {
+                    List<string> contentList = new List<string>();
+                    List<RootObject> newList = await YoutubeEntity.GetYoutube();
+                    for (int i = 0; i < newList.Count; i++)
+                    {
+                        collection.Add(newList[i]);
+                    }
+                    YouyubeGridView.ItemsSource = collection;
+                }
+            }
+            catch (Exception exception)
+            {
+                Debug.WriteLine(exception);
+            }
+        }
+    }
+}
---
+++ UWP/Practical/App1/App1/YoutubeEntity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace App1
+{
+    public class YoutubeEntity
+    {
+        private static readonly string Baseurl = "http://youtube-video-api-1608.appspot.com/youtube/api";
+
+        public static async Task<List<RootObject>> GetYoutube()
+        {
+            var http = new HttpClient();
+            var response = await http.GetAsync(Baseurl);
+            var result = await response.Content.ReadAsStringAsync();
+            List<RootObject> objects = JsonConvert.DeserializeObject<List<RootObject>>(result);
+            return objects;
+        }
+    }
+
+    public class RootObject
+    {
+        public string videoId { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string keywords { get; set; }
+        public string category { get; set; }
+        public string genre { get; set; }
+        public string authorName { get; set; }
+        public string authorEmail { get; set; }
+        public string birthday { get; set; }
+    }
+}
