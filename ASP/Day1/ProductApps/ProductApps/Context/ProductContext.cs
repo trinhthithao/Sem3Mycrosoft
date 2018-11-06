e0e4921a2497c5c27cddc6e47d68dd72d56a11f7
trinhthithao <thaottd00576@fpt.edu.vn>
2018-11-06 12:20:46 +0700

    Add project

---
+++ ASP/Day1/ProductApps/ProductApps/Context/ProductContext.cs
@@ -0,0 +1,16 @@
+﻿using ProductApps.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace ProductApps.Context
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }  
+
+    }
+}
