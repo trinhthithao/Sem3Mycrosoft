fd5c30ba6025e7ea34563f5d523dafa66a9a7a12
trinhthithao <thaottd00576@fpt.edu.vn>
2018-12-02 14:25:07 +0700

    codefirst

---
+++ ASP/Student/Student/Controllers/CourseController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Student.Context;
+
+namespace Student.Controllers
+{
+    public class CourseController : Controller
+    {
+        SchoolContext db = new SchoolContext();
+        // GET: Course
+        public ActionResult Index()
+        {
+            var courses = db.Courses.Include(c => c.Department);
+            return View(courses.ToList());
+        }
+
+        // GET: Course/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Course/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Course/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Course/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Course/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Course/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Course/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ASP/Student/Student/Controllers/InstructorController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Student.Context;
+using Student.Models;
+
+namespace Student.Controllers
+{
+    public class InstructorController : Controller
+    {
+        SchoolContext db = new SchoolContext();
+        // GET: Instructor
+        public ActionResult Index(int? id, int? courseId)
+        {
+            var viewModel = new InstructorIndexData();
+            viewModel.Instructors = db.Instructors
+                .Include(i => i.OfficeAssignment)
+                .Include(i => i.Courses.Select(c => c.Department))
+                .OrderBy(i => i.LastName);
+
+            if (id != null)
+            {
+                ViewBag.InstructorID = id.Value;
+                viewModel.Courses = viewModel.Instructors.Single(i => i.ID == id.Value).Courses;
+            }
+
+            if (courseId != null)
+            {
+                ViewBag.CourseID = courseId.Value;
+                //viewModel.Enrollments = viewModel.Courses.Single(i => i.CourseID == courseId).Enrollments;
+
+                //viewModel.Enrollments = viewModel.Courses.Where(
+                //    x => x.CourseID == courseId).Single().Enrollments;
+
+                var selectedCourse = viewModel.Courses.Where(x => x.CourseID == courseId).Single();
+                db.Entry(selectedCourse).Collection(x => x.Enrollments).Load();
+                foreach (Enrollment enrollment in selectedCourse.Enrollments)
+                {
+                    db.Entry(enrollment).Reference(x => x.Student).Load();
+                }
+
+                viewModel.Enrollments = selectedCourse.Enrollments;
+            }
+            return View(viewModel);
+        }
+    }
+}
